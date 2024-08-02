a9af0a6b1f270b53afe36538b32896da0f7c3fae
Seorim Lee <esr619@naver.com>
2024-08-02 19:59:53 +0900

    로그인페이지 세팅

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,20 +3,25 @@ export default class Login {
     const container = document.createElement("div");
     container.innerHTML = `
         <h1> 
-        <a href="/">
-          <img src="./images/Logo-hodu.png" alt="호두 메인페이지" class="w-[238px] h-[74px]" />
-        </a></h1>
+            <a href="/">
+                <img src="./images/Logo-hodu.png" alt="호두 메인페이지" class="w-[238px] h-[74px]" />
+            </a>
+        </h1>
         <form>
-          <label for="username">Username:</label>
-          <input type="text" id="username" name="username" required />
-          <br />
-          <label for="password">Password:</label>
-          <input type="password" id="password" name="password" required />
-          <br />
-          <button type="submit">Login</button>
+            <label for="username">
+            <input type="text" id="username" name="username" placeholder="아이디" required />
+            </label>
+            <br />
+            <label for="password">
+            <input type="password" id="password" name="password" placeholder="비밀번호" required />
+            </label>
+            <br />
+            <button type="submit">로그인</button>
         </form>
-
-        
+        <ul>
+            <li>회원가입</li>
+            <li>비밀번호 찾기</li>
+        </ul>
       `;
     return container;
   }
