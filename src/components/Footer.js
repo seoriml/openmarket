cc0c6bc56f31d97542bb85c494cc9067b458df5b
Seorim Lee <esr619@naver.com>
2024-08-03 17:03:32 +0900

    안쓰는 코드 제거

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 export default function Header() {
   const footer = document.createElement("footer");
-  footer.className = "footer";
   footer.innerHTML = `
         <p>&copy; 2024 Open Market</p>
       `;
