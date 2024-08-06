294f3230621476ed148e03ffe165c78969da5a5f
Seorim Lee <esr619@naver.com>
2024-08-06 11:25:29 +0900

    로그인을 제외한 페이지에 헤더푸터 컴포넌트 추가

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import "/src/style.css";
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
 import Home from "./pages/home.js";
 import Login from "./pages/login.js";
+import Cart from "./pages/cart.js";
 
 const routes = {
   home: Home,
   login: Login,
+  cart: Cart,
 };
 
 async function renderPage() {
@@ -16,7 +20,14 @@ async function renderPage() {
   try {
     const page = routes[hash] || NotFound;
     const renderedPage = await page();
+
+    if (hash !== "login") {
+      const header = Header();
+      content.appendChild(header);
+    }
     content.appendChild(renderedPage);
+    const footer = Footer();
+    content.appendChild(footer);
   } catch (error) {
     console.error("페이지 렌더링 중 오류 발생:", error);
     content.innerHTML = "<p>페이지를 로드하는 중 오류가 발생했습니다.</p>";
