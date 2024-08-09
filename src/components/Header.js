d107c0b24a9ce0bd8518b3ea76a35fc3e1ecbd18
Seorim Lee <esr619@naver.com>
2024-08-09 17:44:20 +0900

    변수이름 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -117,7 +117,7 @@ export default function Header() {
 
     <div id="loginModal" class="modal delete-modal fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center hidden">
       <div class="modal-content bg-white pt-[50px] pb-[40px] border border-[#767676] w-full max-w-[360px] text-center relative">
-      <button id="modalClose" class="absolute top-[18px] right-[18px] w-[22px] h-[22px] bg-[url('/src/images/icon-delete.svg')] bg-no-repeat bg-center bg-contain">
+        <button id="modalClose" class="absolute top-[18px] right-[18px] w-[22px] h-[22px] bg-[url('/src/images/icon-delete.svg')] bg-no-repeat bg-center bg-contain">
       </button> 
       <h2>로그인이 필요한 서비스입니다</h2>
         <p>로그인하시겠습니까?</p>
@@ -140,9 +140,9 @@ export default function Header() {
 
   // 비회원 사용자가 장바구니 버튼을 클릭했을 때의 핸들러
   if (userStatus === "guest") {
-    const cartLink = header.querySelector('a[href="#cart"]');
-    if (cartLink) {
-      cartLink.addEventListener("click", (e) => {
+    const cartButton = header.querySelector('a[href="#cart"]');
+    if (cartButton) {
+      cartButton.addEventListener("click", (e) => {
         e.preventDefault();
         const modal = document.getElementById("loginModal");
         modal.classList.remove("hidden");
