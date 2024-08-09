23703e9bf5d7fc8e1f83a3da62bc53395ffd9a9d
Seorim Lee <esr619@naver.com>
2024-08-09 17:50:39 +0900

    사용하지않는 주석 제거

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#21BF48", // 주 색상
-        secondary: "#767676", // 보조 색상
-        background: "#f5f5f5", // 배경 색상
-        border: "#767676", // 테두리 색상
-        text: "#333333", // 텍스트 색상
+        primary: "#21BF48",
+        secondary: "#767676",
+        background: "#f2f2f2",
+        border: "#767676",
+        text: "#333333",
       },
     },
   },
