c97dcbdceb11b1ed7f042fe3bf454d7aed15cce9
Seorim Lee <esr619@naver.com>
2024-08-11 14:46:09 +0900

    대체텍스트 수정

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -19,11 +19,12 @@ export default async function ProductList() {
         <li data-product-id="${product.product_id}" class="product-item">
           <a href="/openmarket/#detail/${product.product_id}">
             <image src="${product.image}" alt="${
-        product.product_info
+        product.product_name
       }" class="w-full h-full max-w-[380px] max-h-[380px] object-cover border border-[#c4c4c4] rounded-[10px]">
-          <p class="text-[16px] text-[#767676] mt-[16px]">${
-            product.store_name
-          }</p>
+          <p class="text-[16px] text-[#767676] mt-[16px]">${product.store_name.replace(
+            /\x08/g,
+            ""
+          )}</p>
           <h2 class="text-[18px] mt-[10px]">${product.product_name}</h2>
           <p class="text-[18px] mt-[10px]"><b class="text-[24px] font-bold">${product.price.toLocaleString()}</b> 원</p>
           </a>
