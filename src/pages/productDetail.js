f3ee7554c40779076a0f4f3a55e2d55ff5d34e7e
Seorim Lee <esr619@naver.com>
2024-08-11 14:50:56 +0900

    대체텍스트 수정

--- src/pages/productDetail.js
+++ src/pages/productDetail.js
@@ -22,10 +22,13 @@ export default async function ProductDetail(productId) {
     detailContainer.innerHTML = `
         <h1 class="text-[24px] font-bold mb-[20px]">${product.product_name}</h1>
         <img src="${product.image}" alt="${
-      product.product_info
+      product.product_name
     }" class="w-full max-w-[600px] mb-[20px]">
         <p class="text-[18px] mb-[10px]">가격: ${product.price.toLocaleString()} 원</p>
-        <p class="text-[16px] mb-[10px]">판매처: ${product.store_name}</p>
+        <p class="text-[16px] mb-[10px]">판매처: ${product.store_name.replace(
+          /\x08/g,
+          ""
+        )}</p>
         <p class="text-[16px]">${product.product_info}</p>
       `;
 
