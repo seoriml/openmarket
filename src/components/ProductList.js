5e3d35ba3e4f3643c476a8ffce9dafa03adabad9
Seorim Lee <esr619@naver.com>
2024-08-11 16:11:06 +0900

    상품목록 컴포넌트 주석추가

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,21 @@
 import { url } from "../main";
 
+/* API로부터 상품 목록을 가져오는 비동기 함수 */
+async function fetchProductList() {
+  try {
+    const response = await fetch(`${url}/products/`);
+    if (!response.ok)
+      throw new Error("상품 데이터를 가져오는 데 실패했습니다.");
+
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+/* 상품 목록 생성 비동기 함수 */
 export default async function ProductList() {
   const container = document.createElement("section");
   container.className = "product-list-page max-w-container m-auto py-[80px]";
@@ -31,25 +47,11 @@ export default async function ProductList() {
         </li>
         `
     )
-    .join("");
+    .join(""); // 모든 상품 HTML 문자열을 하나의 문자열로 결합
 
   container.innerHTML = `
     <ul class="products grid grid-cols-3 gap-[70px]">${productListHTML}</ul>
   `;
 
   return container;
-}
-
-async function fetchProductList() {
-  try {
-    const response = await fetch(`${url}/products/`);
-    if (!response.ok)
-      throw new Error("상품 데이터를 가져오는 데 실패했습니다.");
-
-    const data = await response.json();
-    return data.results;
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
 }
